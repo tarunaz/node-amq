ced0e19d2255cc46527053e2a74875a963a75a01
Mehra <MehraT@na.cobank2.corp>
2019-02-01 10:36:07 -0700

    my first commit

---
+++ application.js
@@ -0,0 +1,26 @@
+// var stompit = require('stompit');
+// var http = require('http') ;
+var port = 3000;
+const express = require('express');
+const app = (module.exports = express());
+const bodyParser = require('body-parser');
+
+const log = require('tke-logger').getLogger(__filename);
+
+app.use('/publish', require('lib/routes/publish.js'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+log.info('starting application');
+
+
+// 404 handler
+app.use(function notFoundHandler(req, res) {
+  res.status(404).json({
+    message: '404 not found'
+  });
+});
+
+app.listen(port, function onListening() {
+  log.info('App started at: ' + new Date() + ' on port: ' + port);
+});
